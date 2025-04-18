e32953732e672bbc35c4c975fc29bf9ccb33e526
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-04-18 13:19:19 +0630

    testing purpose blade simple

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head><title>API Docs</title></head>
+<body>
+    <h1>📘 API Visibility - Docs</h1>
+    <p>This is a placeholder. You'll see auto-generated docs here soon.</p>
+</body>
+</html>
---
+++ resources/views/preview.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head><title>API Preview</title></head>
+<body>
+    <h1>👀 API Preview</h1>
+    <p>Response preview goes here.</p>
+</body>
+</html>
