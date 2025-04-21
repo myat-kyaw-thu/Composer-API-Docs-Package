cae7894587fb0d29ea0f3f3cec9ed8741cb0d319
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-04-21 20:04:53 +0630

    project clean up and re set up again

--- resources/views/docs.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>API Docs</title></head>
-<body>
-    <h1>📘 API Visibility - Docs</h1>
-    <p>This is a placeholder. You'll see auto-generated docs here soon.</p>
-</body>
-</html>
--- resources/views/preview.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>API Preview</title></head>
-<body>
-    <h1>👀 API Preview</h1>
-    <p>Response preview goes here.</p>
-</body>
-</html>
