1507ded30444764170648ba80b3f415033e7892b
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-04-23 20:18:58 +0630

    Facade manager

---
+++ src/FacadeManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Primebeyonder\LaravelApiVisibility;
+
+use Primebeyonder\LaravelApiVisibility\Contracts\DocumentationGeneratorInterface;
+use Primebeyonder\LaravelApiVisibility\Contracts\ResponsePreviewerInterface;
+
+class FacadeManager
+{
+    /**
+     * @var DocumentationGeneratorInterface
+     */
+    protected $documentationGenerator;
+
+    /**
+     * @var ResponsePreviewerInterface
+     */
+    protected $responsePreviewer;
+
+    /**
+     * Create a new facade manager instance.
+     *
+     * @param DocumentationGeneratorInterface $documentationGenerator
+     * @param ResponsePreviewerInterface $responsePreviewer
+     */
+    public function __construct(
+        DocumentationGeneratorInterface $documentationGenerator,
+        ResponsePreviewerInterface $responsePreviewer
+    ) {
+        $this->documentationGenerator = $documentationGenerator;
+        $this->responsePreviewer = $responsePreviewer;
+    }
+
+    /**
+     * Get the API documentation.
+     *
+     * @return array
+     */
+    public function getDocumentation()
+    {
+        return $this->documentationGenerator->generate();
+    }
+
+    /**
+     * Preview a response for a route.
+     *
+     * @param string $routeName
+     * @param array $parameters
+     * @return mixed
+     */
+    public function previewResponse(string $routeName, array $parameters = [])
+    {
+        return $this->responsePreviewer->preview($routeName, $parameters);
+    }
+}
