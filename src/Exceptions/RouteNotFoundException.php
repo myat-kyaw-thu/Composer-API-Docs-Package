39f976a6f44c8eabb3a74a1113cb72aa16a9b9f9
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-05-01 21:59:25 +0630

    Create Custom Exception Class for 404 error  Exception

---
+++ src/Exceptions/RouteNotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Primebeyonder\LaravelApiVisibility\Exceptions;
+
+class RouteNotFoundException extends ApiVisibilityException
+{
+    /**
+     * Create a new exception instance.
+     *
+     * @param string $routeName
+     * @return void
+     */
+    public function __construct(string $routeName)
+    {
+        parent::__construct("Route [{$routeName}] not found.");
+
+        $this->setSuggestions([
+            "Check if the route name '{$routeName}' is correct.",
+            "Ensure the route is registered in your routes file.",
+            "Verify the route is not excluded in the api-visibility config.",
+        ]);
+    }
+}
