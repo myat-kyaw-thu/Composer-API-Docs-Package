a925e3c82db41bb68d31e7bc7550fb76f246ed95
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-05-01 21:59:10 +0630

    Create Custom Exception Class for Invalid Res Error Exception

---
+++ src/Exceptions/InvalidResponseException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Primebeyonder\LaravelApiVisibility\Exceptions;
+
+class InvalidResponseException extends ApiVisibilityException
+{
+    /**
+     * Create a new exception instance.
+     *
+     * @param string $message
+     * @param \Throwable|null $previous
+     * @return void
+     */
+    public function __construct(string $message, \Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+
+        $this->setSuggestions([
+            "Check your controller's return value.",
+            "Ensure your JSON is properly formatted.",
+            "Verify that your response is properly serializable.",
+            "Check for any database connection issues if fetching data.",
+        ]);
+    }
+}
